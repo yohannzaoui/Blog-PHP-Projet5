8f108417922ce11f5308c7e082b9305f10100330
yohannzaoui <yohannzaoui@gmail.com>
2018-07-20 10:29:29 +0200

    add classes

---
+++ models/Manager.php
@@ -0,0 +1,13 @@
+<?php
+
+class Manager
+{
+  
+
+  public function dbConnect()
+  {
+    $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8','root','');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    return $db;
+  }
+}
