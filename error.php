3794b79616d282be1f358b43896455f128fe37f9
yohannzaoui <yohannzaoui@gmail.com>
2018-07-20 09:29:49 +0200

    delete

--- error.php
+++
@@ -1,7 +0,0 @@
-<?php $title = 'Yohann Zaoui - Le Blog - Erreur'; ?>
-
-<?php ob_start() ?>
-<p>Une erreur est survenue : <?= $msgError ?></p>
-<?php $content = ob_get_clean(); ?>
-
-<?php require('template.php'); ?>
