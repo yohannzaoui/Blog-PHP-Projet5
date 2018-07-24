5f816c89a360ed8884c19a4840d2ea845c183bd6
yohannzaoui <yohannzaoui@gmail.com>
2018-07-24 20:37:53 +0200

    update design

--- views/template.php
+++ views/template.php
@@ -5,11 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?= $title ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="public/css/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="public/bootstrap/css/bootstrap.css" />
+    <link href="public/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 </head>
 <body>
-    <h1><a href="<?=URL?>">MON BLOG</a></h1>
+    
+    <?php require('navbar.php'); ?>
+    <?php require('header.php'); ?>
+
+    
     <?= $content ?>
+
+    <?php require('footer.php'); ?>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="public/jquery/jquery.min.js"></script>
+    <script src="public/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="public/js/jqBootstrapValidation.js"></script>
+    <script src="public/js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="public/js/clean-blog.min.js"></script>
 </body>
 </html>
--- views/viewHome.php
+++ views/viewHome.php
@@ -2,13 +2,31 @@
 
 $this->_title="Le BLOG";
 
+require('about.php');
+
 foreach($posts as $post) : ?>
 
-<h2><?= $post->title() ?></h2>
-<p><?= $post->content() ?></p>
-<p><?= $post->author() ?></p>
-<p><?= $post->creation_date() ?></p>
-
+<div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-10 mx-auto">
+          <div class="post-preview">
+            <a href="posts.php?id="<?=$post->id()?>">
+              <h2 class="post-title">
+              <?= $post->title() ?>
+              </h2>
+              <h3 class="post-subtitle">
+              <p><?= $post->content() ?>
+              </h3>
+            </a>
+            <p class="post-meta">Ecrit par le 
+              <a href=""><?= $post->author() ?></a>
+              <?= $post->creation_date() ?></p>
+          </div>
+          <hr>
+        </div>
+      </div>
+    </div>
 
 
 <?php endforeach ?>
+
