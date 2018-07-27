afc5372d8e33947a748c4da96272195062131f6b
yohannzaoui <yohannzaoui@gmail.com>
2018-07-27 21:01:03 +0200

    add comment form

--- views/viewPost.php
+++ views/viewPost.php
@@ -1,3 +1,5 @@
+<?php $this->_title = $post->getTitle(); ?>
+
 <div class="container">
       <div class="alert alert-primary" role="alert">
         <div class="row">
@@ -13,9 +15,39 @@
             <p class="post-meta">
             <?php echo 'Article de '.$post->getAuthor().' le '.$post->getCreation_date().' -- Modifié le '.$post->getUpdate_date() ?>
             </p>
-            <p><small><a href="listPost.php">Retour à la liste d'articles</a></small></p>
+            <p><small><a href="index.php?url=ListPosts">Retour à la liste d'articles</a></small></p>
           </div>
         </div>
       </div>
     </div>
   </div>
+
+<div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-10 mx-auto">
+          <h2 class="title_center">Commentaires</h2>
+          <form method="post" action="" name="sentMessage" id="contactForm" novalidate>
+            <div class="control-group">
+              <div class="form-group col-xs-12 floating-label-form-group controls">
+                <label>Votre pseudo</label>
+                <input type="text" class="form-control" placeholder="Votre pseudo" id="pseudo" required data-validation-required-message="SVP entrez votre pseudo.">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+            <div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Votre commentaire</label>
+                <textarea rows="5" class="form-control" placeholder="Votre commentaire" id="comment" required data-validation-required-message="SVP entrez votre commentaire."></textarea>
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+            <br>
+            <div id="success"></div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary" id="sendMessageButton">Envoyer</button>
+              <button type="reset" class="btn btn-danger" id="sendMessageButton">Effacer</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
