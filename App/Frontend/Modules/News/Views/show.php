c83e59b5c628f893cba6972941886b005c984eac
yohannzaoui <yohannzaoui@gmail.com>
2018-07-30 09:24:59 +0200

    update code

--- App/Frontend/Modules/News/Views/show.php
+++ App/Frontend/Modules/News/Views/show.php
@@ -30,13 +30,7 @@
 
 
 
-<p>Par <em><?= $news['auteur'] ?></em>, le <?= $news['dateAjout']->format('d/m/Y à H\hi') ?></p>
-<h2><?= $news['titre'] ?></h2>
-<p><?= nl2br($news['contenu']) ?></p>
 
-<?php if ($news['dateAjout'] != $news['dateModif']) { ?>
-  <p style="text-align: right;"><small><em>Modifiée le <?= $news['dateModif']->format('d/m/Y à H\hi') ?></em></small></p>
-<?php } ?>
 
 <p><a href="commenter-<?= $news['id'] ?>.html">Ajouter un commentaire</a></p>
 
