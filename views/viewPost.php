c08f3e47f45b3c2a552669344228d24ca8c4fc2e
yohannzaoui <yohannzaoui@gmail.com>
2018-07-31 10:27:43 +0200

    add comments

--- views/viewPost.php
+++ views/viewPost.php
@@ -19,3 +19,43 @@
       </div>
     </div>
   </div>
+
+  <div class="container">
+   <div class="row">
+     <div class="col-lg-12 col-md-10 mx-auto">
+       <h2 class="title_center">Ecrire un commentaire</h2>
+       <form action="" method="post">
+         <div class="control-group">
+           <div class="form-group floating-label-form-group controls">
+             <label for="pseudo">Auteur</label>
+             <input type="text" class="form-control" name="pseudo" placeholder="Votre pseudo" id="pseudo" required data-validation-required-message="SVP Entrez votre pseudo.">
+             <p class="help-block text-danger"></p>
+           </div>
+         </div>
+         <div class="control-group">
+           <div class="form-group floating-label-form-group controls">
+             <label for="message">Commentaire</label>
+             <textarea rows="5" class="form-control" name="content" placeholder="Message" id="message" required data-validation-required-message="SVP Entrez votre commentaire."></textarea>
+             <p class="help-block text-danger"></p>
+           </div>
+         </div>
+         <br>
+         <div class="form-group">
+           <button type="submit" name="sendComment" class="btn btn-primary" id="sendMessageButton">Envoyer</button>
+           <button type="reset" class="btn btn-danger" id="sendMessageButton">Effacer</button>
+         </div>
+       </form>
+
+
+<div class="container">
+   <div class="row">
+     <div class="col-lg-12 col-md-10 mx-auto">
+       <h2 class="title_center">Les commentaire</h2>
+       <?php foreach($comments as $comment) : ?>
+          <p><strong><?=$comment->getPseudo() ?></strong> le <?= $comment->getCreation_date() ?></p>
+            <p><?=$comment->getContent() ?></p>
+            <hr>
+            <?php endforeach ?>
+        </div>
+      </div>
+</div>
