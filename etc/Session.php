ba54b384fe666eaf4f518d3aa9f153990d51c484
yohannzaoui <yohannzaoui@gmail.com>
2018-08-23 17:56:27 +0200

    update session class

--- etc/Session.php
+++ etc/Session.php
@@ -5,37 +5,37 @@ namespace Core;
 class Session
 {
 
-    public function start()
+    public function sessionStart()
     {
         session_start();
     }
 
     
-    public function destroy()
+    public function sessionDestroy()
     {
         session_destroy();
     }
 
     
-    public function setAttribut($nom, $valeur)
+    public function setSession($name, $value)
     {
-        $_SESSION[$nom] = $valeur;
+        $_SESSION[$name] = $value;
     }
 
     
-    public function existeAttribut($nom)
+    public function existeSession($name)
     {
-        return (isset($_SESSION[$nom]) && $_SESSION[$nom] != "");
+        return (isset($_SESSION[$name]) && $_SESSION[$name] != "");
     }
 
     
-    public function getAttribut($nom)
+    public function getSession($name)
     {
-        if ($this->existeAttribut($nom)) {
-            return $_SESSION[$nom];
+        if ($this->existeAttribut($name)) {
+            return $_SESSION[$name];
         }
         else {
-            throw new Exception("Attribut '$nom' absent de la session");
+            throw new \Exception("Attribut '$name' absent de la session");
         }
     }
 
