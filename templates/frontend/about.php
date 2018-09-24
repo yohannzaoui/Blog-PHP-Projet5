7acf53a3a00c26e79eb8dc199e8b156325318221
yohannzaoui <yohannzaoui@gmail.com>
2018-09-24 12:16:21 +0200

    update text

--- templates/frontend/about.php
+++ templates/frontend/about.php
@@ -3,17 +3,19 @@
     <h2 class="title_center">A propos</h2>
     <div class="alert alert-dark" role="alert">
       <div class="row">
-        <div class="col-lg-10 col-md-10 mx-auto">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit,
-            fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error,
-            repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus,
-            minus!</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque
-            esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
-        </div>
-        <div class="col-lg-2 col-md-2 mx-auto" >
+      <div class="col-lg-2 col-md-2 mx-auto" >
         <p class="title_center"><img src="/images/avatar_black.png" alt="Avatar Yohann Zaoui" class="img-thumbnail"></p>
+        </div>
+        <div class="col-lg-10 col-md-10 mx-auto">
+          <h4>Bonjour et bienvenue sur mon blog</h4>
+          <p>Je m'appelle Yohann Zaoui, et je suit actuellement une formation pour devenir développeur d'application PHP/Symphony grâce à OpenclassRooms. J'ai débuté cette formation au mois de d'avril 2017.</p>
+          <p>La formation débute par l'apprentissage des languages HTML et CSS 3 qui sont les bases à connaitre pour un developpeur.</p>
+          <p> - Un premier projet : <strong>"Intégrez un thème Wordpress pour un client"</strong>. réaliser un site web  grace au CMS Wordpress pour une agence immobiliere de chalet de luxe à Couchevel. Le but savoir manipuler le CMS ainsi que les differents plug-in. <a href="http://yohannzaoui.com/" target="_blank">Cliquez pour voir le projet</a></p>
+          <p> - Le second projet : <strong>"Analysez les besoins de votre client pour son Festival de films"</strong>. Création d'une maquette pour le site web de l'association "Les films de plein air".<br>
+          Le but de ce projet est la mis en pratique du langage HTML5, CSS3, l'utilisation du framework CSS Bootstrap, crée un site qui soit responsive, ainsi que la rédaction d'un document de spécification technique. <a href="http://films-de-plein-air.yohannzaoui.com/" target="_blank">Cliquez pour voir le projet</a></p>
+          <p> - Pour le troisième projet : <strong>"Concevez la solution technique d'une application de restauration en ligne, Express Food"</strong>. Elaboration des schémas UML (diagramme de cas d'utilisation, de classe et de séquence ansi que la base de donnée SQL pour une application de livraison de plat par des coursiers à vélo. <a href="https://github.com/yohannzaoui/UML-MPD" target="_blank">Cliquez pour voir le projet sur Github</a></p>
+          <p>Après le troisième projet débute l'apprentissage du langage PHP. Les bases de la concéption d'un site web dynamique grâce à PHP et Mysql, l'architecture MVC pour une organisation pratique des fichiers et la programmation orienté objet indispensable pour un code facilement maintenable.<br>
+          Et pour appliquer toutes ces connaissances le quatrième projet est la <strong>concéption de ce  blog</strong>, je vois laisse naviguez sur celui-ci....Bonne visite. <a href="https://github.com/yohannzaoui/Blog-PHP-Projet5" target="_blank">Cliquez pour voir le projet sur Github</a></p>
         </div>
       </div>
     </div>
