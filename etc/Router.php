794c808bb075fdf2770705a141771e72eb8f9cf0
yohannzaoui <yohannzaoui@gmail.com>
2018-09-27 16:29:10 +0200

    update vendor

--- etc/Router.php
+++ etc/Router.php
@@ -51,8 +51,11 @@ class Router implements RouterInterface
                 $controller = $route->getAction();
                 $class = new $controller();
                 return $class($request);
-            } 
+            }
+            
+            
         }
+        echo "not found";
     }
 
     /**
--- public/index.php
+++ public/index.php
@@ -5,9 +5,7 @@ require_once __DIR__ ."/../vendor/autoload.php";
 use Core\Request;
 use Core\Response;
 use Core\Application;
-
-
-    
+  
 $request = Request::createFromGlobals();
 $request->getSession()->sessionStart();
 $application = new Application();
